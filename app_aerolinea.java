517044690f112c6e911d146f10ca5d7f3572b971
Yeison Cordoba <corba.ye99@gmail.com>
2024-03-06 21:17:35 -0500

    Validando funciones

--- app_aerolinea.java
+++ app_aerolinea.java
@@ -11,36 +11,49 @@ public class app_aerolinea {
 
     private static void fnt_registrar_cliente(){
         String sexo = "";
+        boolean sw = false;
         String id = JOptionPane.showInputDialog(null,"Ingrese el ID del cliente");
-        String nombre = JOptionPane.showInputDialog(null,"Ingrese el Nombre del cliente");
-        String contacto = JOptionPane.showInputDialog(null,"Ingrese el Contacto del cliente");
-        String correo = JOptionPane.showInputDialog(null,"Ingrese el Correo del cliente");
-        int option_sexo = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el sexo\n\n1. Masculino\n2. Femenino \n3.  Otro "));
-        if (option_sexo == 1) {
-            sexo = "Masculino";
-        }else if (option_sexo == 2) {
-            sexo = "Femenino";
-        }else {
-            sexo = "Otro";
+        for(int i = 0; i < pasajeros.size(); i++){
+            if (pasajeros.get(i).getId().equals(id)) {
+                sw = true;
+                break;
+            }
         }
-        int membresia = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el tipo de membresia\n\n 1. BRONCE\n2. PLATA \n3.  ORO \n4. PLATINO"));
-        if (membresia == 1) {
-            membresia = 1;
-        }
-        else if (membresia == 2 ) {
-            membresia = 2;
-        }
-        else if (membresia == 3) {
-            membresia = 3;
-        }
-        else if (membresia == 4) {
-            membresia = 4;
+        if (!sw) {
+            String nombre = JOptionPane.showInputDialog(null,"Ingrese el Nombre del cliente");
+            String contacto = JOptionPane.showInputDialog(null,"Ingrese el Contacto del cliente");
+            String correo = JOptionPane.showInputDialog(null,"Ingrese el Correo del cliente");
+            int option_sexo = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el sexo\n\n1. Masculino\n2. Femenino \n3.  Otro "));
+            if (option_sexo == 1) {
+                sexo = "Masculino";
+            }else if (option_sexo == 2) {
+                sexo = "Femenino";
+            }else {
+                sexo = "Otro";
+            }
+            Integer membresia = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el tipo de membresia\n\n 1. BRONCE\n2. PLATA \n3.  ORO \n4. PLATINO"));
+            if (membresia == 1) {
+                membresia = 1;
+            }
+            else if (membresia == 2 ) {
+                membresia = 2;
+            }
+            else if (membresia == 3) {
+                membresia = 3;
+            }
+            else if (membresia == 4) {
+                membresia = 4;
+            }else{
+                JOptionPane.showMessageDialog(null, "Membresia no valida, asignando por defecto BRONCE");
+                membresia = 1;
+            }
+            if (!id.equals("") && !nombre.equals("") &&!contacto.equals("") &&!correo.equals("") &&!sexo.equals("") && membresia != null) {
+                pasajeros.add(new cls_registro_pasajeros(id, nombre, contacto, correo, sexo, membresia));
+                JOptionPane.showMessageDialog(null, "Cliente registrado con éxito");   
+            }
         }else{
-            JOptionPane.showMessageDialog(null, "Membresia no valida, asignando por defecto BRONCE");
-            membresia = 1;
+            JOptionPane.showMessageDialog(null, "Registro existente encontrado");
         }
-        pasajeros.add(new cls_registro_pasajeros(id, nombre, contacto, correo, sexo, membresia));
-        JOptionPane.showMessageDialog(null, "Cliente registrado con éxito");
 
     }
 
@@ -56,7 +69,7 @@ public class app_aerolinea {
             }
         }
         if (sw) {
-            int tipo_clase = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el tipo de clase\n\n1. Economica: $450,000\n2. Turista: $600,000 \n3. VIP $900,000 "));
+            Integer tipo_clase = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el tipo de clase\n\n1. Economica: $450,000\n2. Turista: $600,000 \n3. VIP $900,000 "));
             if (tipo_clase == 1) {
                 tipo_clase = 1;
             }
@@ -70,10 +83,13 @@ public class app_aerolinea {
                 JOptionPane.showMessageDialog(null, "Tipo de clase no valida, asignando por defecto clase ECONOMICA");
                 tipo_clase = 1;
             }
-            int cantidad = Integer.valueOf(JOptionPane.showInputDialog(null,"Ingrese la cantidad de Tiketes"));
-            int tipo_pago = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el tipo de pago\n\n1. Efectivo \n2. Tarjeta de credito \n3. Tarjeta de ahorros \n4. Transferencia"));
-            compra_tiquetes.add(new cls_compra_tiquetes(id, tipo_clase, cantidad, tipo_pago, descuento_membresia));
-            JOptionPane.showMessageDialog(null, "Compra registrada éxitosamente");
+            Integer cantidad = Integer.valueOf(JOptionPane.showInputDialog(null,"Ingrese la cantidad de Tiketes"));
+            Integer tipo_pago = Integer.valueOf(JOptionPane.showInputDialog(null,"Selecione el tipo de pago\n\n1. Efectivo \n2. Tarjeta de credito \n3. Tarjeta de ahorros \n4. Transferencia"));
+            if (!id.equals("") && tipo_clase != null && cantidad != null && tipo_pago != null) {
+                compra_tiquetes.add(new cls_compra_tiquetes(id, tipo_clase, cantidad, tipo_pago, descuento_membresia));
+                JOptionPane.showMessageDialog(null, "Compra registrada éxitosamente");
+            }
+
         }else{
             JOptionPane.showMessageDialog(null, "No se encontraron registros del cliente");
         }
